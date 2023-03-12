9df324f682550ce5416703479ad4804ec54f17e7
asif0927 <6zd6jws@code.edu.az>
2023-03-12 18:38:19 +0400

    fibonnaci numbers created

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int count = int.Parse(Console.ReadLine());
+Console.Write(a + " " + b);
+
+for (int i = 2; i < count; i++)
+{
+    int c = a + b;
+    Console.Write(" " + c);
+    a = b;
+    b = c;
+}
+Console.WriteLine();
